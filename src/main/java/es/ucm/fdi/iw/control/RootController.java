b8ef5f941eba07a62afa907d63dc563b1d63fe8f
Diego Romero <36675478+diegorhs@users.noreply.github.com>
2019-03-26 15:51:20 +0100

    Realizada vista de archivo dinámica
    Controlador de archivo está incompleto.

--- src/main/java/es/ucm/fdi/iw/control/RootController.java
+++ src/main/java/es/ucm/fdi/iw/control/RootController.java
@@ -100,6 +100,11 @@ public class RootController {
 		return "share";
 	}
 	
+	@GetMapping("/file")
+	public String file(Model model) {
+		return "file";
+	}
+	
 	/*
 	@GetMapping("/admin")
 	public String admin(Model model, Principal principal) {
