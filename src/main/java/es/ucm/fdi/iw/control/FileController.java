f53ae8bf633679158898b4298c911728b5df100e
Diego Romero <36675478+diegorhs@users.noreply.github.com>
2019-03-29 11:32:11 +0100

    Añadido getFile al controlador de archivo
    Pendiente de incluir metadatos.

--- src/main/java/es/ucm/fdi/iw/control/FileController.java
+++ src/main/java/es/ucm/fdi/iw/control/FileController.java
@@ -1,20 +1,17 @@
 package es.ucm.fdi.iw.control;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Date;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
-import javax.websocket.Session;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,20 +22,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import es.ucm.fdi.iw.LocalData;
+import es.ucm.fdi.iw.model.CFile;
 import es.ucm.fdi.iw.model.Tag;
 import es.ucm.fdi.iw.model.User;
-import es.ucm.fdi.iw.parser.UserParser;
-import es.ucm.fdi.iw.serializer.UserSerializer;
-import es.ucm.fdi.iw.service.UserService;
-import es.ucm.fdi.iw.transfer.UserTransfer;
-import es.ucm.fdi.iw.util.DateUtil;
 import es.ucm.fdi.iw.util.StringUtil;
 
 @Controller()
@@ -75,18 +65,48 @@ public class FileController {
 	public String getFile(ModelAndView modelAndView, HttpSession session, @PathVariable("id") Long id, @ModelAttribute ("userId") Long userId) throws IOException {
 		
 		String err = "File not found";
-
-		Integer index = entityManager.find(Integer.class, id);
-		File f = localData.getFile("file/user"+userId, ""+index);
 		
-		if (f.exists()) {
+		CFile f = entityManager.find(CFile.class, id);
+		
+		if (f != null) {
 			err = null;
-			InputStream in = new BufferedInputStream(new FileInputStream(f));
+			modelAndView.addObject("filename", id);
+			
+			String p = "/user" + userId + "/" + id;
+			modelAndView.addObject("fileurl", p);
+			
+			Path path = Paths.get(p);
+			String mimetype = Files.probeContentType(path);
+			switch (mimetype) {
+			case "image/jpeg":
+			case "image/png":
+				modelAndView.addObject("mimetype", "image");
+				break;
+			case "audio/mpeg":
+				modelAndView.addObject("mimetype", "music");
+				break;
+			case "video/mp4":
+				modelAndView.addObject("mimetype", "video");
+				break;
+			default:
+				err = "File has invalid MIME type";
+			}
+
+			List<Tag> t = f.getTags();
+			List<String> tags = new ArrayList<String>();
+			for (int i = 0; i < t.size(); i++) {
+				tags.add(t.get(i).getName());
+			}
+			modelAndView.addObject("tags", tags);
+			
+			String md = f.getMetadata();
+			Map<String, String> metadata = new HashMap<String, String>();
 			// TODO
+			modelAndView.addObject("metadata", metadata);
 		}
 
 		if (err != null)
-			return "redirect:/";
+			return "redirect:/user/";
 		else return "file";
 	}
 	
