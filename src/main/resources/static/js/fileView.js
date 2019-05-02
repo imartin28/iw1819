e10ee54a56d1328b7bbf9b63a0a1948b0cd83dfc
Diego Romero <36675478+diegorhs@users.noreply.github.com>
2019-05-02 23:44:57 +0200

    Corregido error en vista de archivo

--- src/main/resources/static/js/fileView.js
+++ src/main/resources/static/js/fileView.js
@@ -9,9 +9,9 @@ function initMetadata() {
 	for (let key in metadata) {
 		let title = $('<div class="title"></div>');
 		title.text(key);
-		$('#metadata').append(title);
+		$('#currentMetadata').append(title);
 		let description = $('<div class="description"></div>');
 		description.text(metadata[key]);
-		$('#metadata').append(description);
+		$('#currentMetadata').append(description);
 	}
 };
