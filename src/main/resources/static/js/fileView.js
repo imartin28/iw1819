102f758ac11949e12c82a29e922136b74cbea140
Diego Romero <36675478+diegorhs@users.noreply.github.com>
2019-05-02 23:39:00 +0200

    Actualizada modificación de metadatos
    Añadida posibilidad de eliminar metadatos ya existentes.
    Corregidos errores menores.

--- src/main/resources/static/js/fileView.js
+++ src/main/resources/static/js/fileView.js
@@ -6,13 +6,12 @@ $(function () {
 });
 
 function initMetadata() {
-	let table = $('#metadata');
 	for (let key in metadata) {
 		let title = $('<div class="title"></div>');
 		title.text(key);
-		table.append(title);
+		$('#metadata').append(title);
 		let description = $('<div class="description"></div>');
 		description.text(metadata[key]);
-		table.append(description);
+		$('#metadata').append(description);
 	}
 };
--- src/main/resources/static/js/modifyFile.js
+++ src/main/resources/static/js/modifyFile.js
@@ -7,16 +7,24 @@ $(function() {
 	
 	$('#addMetadata').on("click", function() {
 		if (addMetadata()) {
-			let elem = $('#editMetadata');
-			elem.append('<input type="text" id="keyMetadata-' + count + '">');
-			elem.append('<input type="text" id="valueMetadata-' + count + '">');
+			let button = $('<input type="checkbox" id="buttonMetadata-' + count + '">');
+			button.css("visibility", "hidden");
+			$('#editMetadata').append(button);
+			$('#editMetadata').append('<input type="text" id="keyMetadata-' + count + '">');
+			$('#editMetadata').append('<input type="text" id="valueMetadata-' + count + '">');
 		}
+	});
+	
+	$('#removeMetadata').on("click", function() {
+		for (let i = 0; i < count; i++)
+			removeMetadata(i);
 	});
 	
 });
 
 function initMetadata() {
 	for (let key in metadata) {
+		$('#editMetadata').append($('<input type="checkbox" id="buttonMetadata-' + count + '">'));
 		let keyElem = $('<input type="text" id="keyMetadata-' + count + '">');
 		keyElem.val(key);
 		$('#editMetadata').append(keyElem);
@@ -24,20 +32,42 @@ function initMetadata() {
 		valueElem.val(metadata[key]);
 		$('#editMetadata').append(valueElem);
 		count++;
+		keyElem.prop('disabled', true);
+		valueElem.prop('disabled', true);
 	}
+	let button = $('<input type="checkbox" id="buttonMetadata-' + count + '">');
+	button.css("visibility", "hidden");
+	$('#editMetadata').append(button);
 	$('#editMetadata').append($('<input type="text" id="keyMetadata-' + count + '">'));
 	$('#editMetadata').append($('<input type="text" id="valueMetadata-' + count + '">'));
 	$('#newMetadata').val(JSON.stringify(metadata));
 };
 
 function addMetadata() {
+	let button = $('#buttonMetadata-' + count);
 	let key = $('#keyMetadata-' + count);
 	let value = $('#valueMetadata-' + count);
 	if (key.val() != "" && value.val() != "") {
 		metadata[key.val()] = value.val();
+		button.css("visibility", "visible");
+		key.prop('disabled', true);
+		value.prop('disabled', true);
 		$('#newMetadata').val(JSON.stringify(metadata));
 		count++;
 		return true;
 	}
 	else return false;
-}
+};
+
+function removeMetadata(index) {
+	let button = $('#buttonMetadata-' + index);
+	let key = $('#keyMetadata-' + index);
+	let value = $('#valueMetadata-' + index);
+	if (button.prop('checked')) {
+		delete metadata[key.val()];
+		button.remove();
+		key.remove();
+		value.remove();
+		$('#newMetadata').val(JSON.stringify(metadata));
+	}
+};
