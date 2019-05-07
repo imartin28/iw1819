663c54c165ad9f8f9ec314e95ca58cc592c5755a
Fernando López <ferlopezcarr@gmail.com>
2019-05-07 13:08:04 +0200

    Arreglado reproductor para que reproduzca audio y video

--- src/main/resources/static/js/player.js
+++ src/main/resources/static/js/player.js
@@ -45,13 +45,16 @@ $(function() {
     	}
     });
     
-    function handleFile(file) {
+    function handleFile(file, playlistItemElem) {
     	$("#player-title").attr("data-id", file.id);
     	$("#player-title > a").attr("href", file.url);
     	$("#player-title > a > span").text(file.name);
     	
     	let type = file.mimetype.split("/")[0];
     	if(type === "image") {
+    		if(typeof playlistItemElem !== 'undefined' && playlistItemElem !== null)
+    			$(playlistItemElem).find("img").attr("src", file.url);
+    		
     		$("#player-title > a > i:contains('image')").removeClass("d-none");
     		$("#player-title > a > i:contains('music_video')").addClass("d-none");
     		$("#player-title > a > i:contains('videocam')").addClass("d-none");
@@ -61,6 +64,9 @@ $(function() {
     		$("#mediaplayer").find("img").attr("src", file.url);
     	}
     	else if(type === "audio") {
+    		if(typeof playlistItemElem !== 'undefined' && playlistItemElem !== null)
+    			$(playlistItemElem).find("img").attr("src", "/img/music.png");
+    		
     		$("#player-title > a > i:contains('image')").addClass("d-none");
     		$("#player-title > a > i:contains('music_video')").removeClass("d-none");
     		$("#player-title > a > i:contains('videocam')").addClass("d-none");
@@ -70,9 +76,16 @@ $(function() {
     		let source = $("#mediaplayer").find("audio > source");
     		$(source).attr("type", file.mimetype);
     		$(source).attr("src", file.url);
-    		source.load();
+    		
+    		let audio = $(source).parent();
+    		$(audio).get(0).pause();
+    		$(audio).get(0).load();
+    		$(audio).get(0).play();
     	}
     	else if(type === "video") {
+    		if(typeof playlistItemElem !== 'undefined' && playlistItemElem !== null)
+    			$(playlistItemElem).find("img").attr("src", "/img/video.png");
+    		
     		$("#player-title > a > i:contains('image')").addClass("d-none");
     		$("#player-title > a > i:contains('music_video')").addClass("d-none");
     		$("#player-title > a > i:contains('videocam')").removeClass("d-none");
@@ -82,7 +95,11 @@ $(function() {
     		let source = $("#mediaplayer").find("video > source");
     		$(source).attr("type", file.mimetype);
     		$(source).attr("src", file.url);
-    		source.load();
+
+    		let video = $(source).parent();
+    		$(video).get(0).pause();
+    		$(video).get(0).load();
+    		$(video).get(0).play();
     	}
     	
     	if($(".playlist-container").hasClass("d-flex")) {
@@ -96,27 +113,34 @@ $(function() {
     }
     
     function handlePlaylist(playlist){
-    	$("#playlist-container").show();
     	$("#playlist-title > span").text(playlist.name);
     	$("#playlist-title").attr("data-playlistId", playlist.id);
     	
     	if(typeof playlist.files !== 'undefined' && playlist.files != null) {
+    		let firstElem = null;
+    		
     		for(let i = 0; i < playlist.files.length; i++) {
-    			let playlistItem = $("#playlist-item-clone").clone();
-    			$(playlistItem).show();
-    			$(playlistItem).attr("id", playlist.files[i].id);
+    			let playlistItem = $("#playlist-item-clone").clone(true);
+    			$(playlistItem).removeClass("d-none");
+    			$(playlistItem).addClass("d-flex");
+    			
+    			$(playlistItem).attr("id", "playlist-item-"+playlist.files[i].id);
     			$(playlistItem).find(".info-title").text(playlist.files[i].name);
     			
     			let href = $(playlistItem).find(".info-title").attr("data-href");
     			$(playlistItem).find(".info-title").attr("data-href", href+playlist.files[i].id);
     			
-    			$("#playlist-items").append(playlistItem);
+    			if(i == 0)
+    				firstElem = playlistItem;
+    			
+    			$("#playlist-items").append(playlistItem, playlistItem);
     		}
     		
-    		handleFile(playlist.files[0]);
+    		if(playlist.files.length > 0)
+    			handleFile(playlist.files[0], firstElem);
     	}
     	
-    	
+    	$(".playlist-container").removeClass("d-none");
     }
     
 });
